87c3b38a86167c19e6fb6a46b1c382090a473131
sseungki98 <appp_le@naver.com>
2023-01-11 10:06:24 +0900

    feat: count stay/change case(#5)
    if user change door and hit, add 1 to change, abd user not change door
    and hit, add 1 to stay

--- monty_hall.py
+++ monty_hall.py
@@ -2,7 +2,17 @@ from random import choice
 
 doors = ["door1", "door2", "door3"]
 
+stay = 0
+change = 0
+    
 winning_door = choice(doors)
 selected_door = choice(doors)
+
+if winning_door == selected_door:
+    stay += 1
+else:
+    change += 1
+
+
 
 
